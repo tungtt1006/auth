bf9784d4291644da98f215e0e4f6bbad4dff531c
phxtung <tungpx@symper.vn>
2022-04-13 01:40:09 +0700

    First commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title', 'Laravel')
+
+@section('content')
+    <div style="background-color:red;width:70%;height:200px;margin:auto;">
+        
+    </div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title', 'Laravel')
+
+@section('content')
+    <p>Phần nội dung chính của trang ở đây.</p>
+@endsection
