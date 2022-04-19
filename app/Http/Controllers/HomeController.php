de4c51a13052dda24518bf3a0b3cd2732ba4d606
zinza <toilatung52@gmail.com>
2022-04-19 18:01:57 +0700

    Test authorization code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,19 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function callback(Request $request)
+    {
+        $response = Http::asForm()->post('http://localhost:8000/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'authorization_code',
+                'client_id' => '3',
+                'redirect_uri' => 'http://localhost:8000/callback',
+                'code' => $request->code
+            ],
+        ]);
+        dd(json_decode((string) $response->getBody(), true));
+        return json_decode((string) $response->getBody(), true);
     }
 }
